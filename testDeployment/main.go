b294764761e34696f0e81d58d1b546f89d6f8cb3
[Theo Barber-Bany] <theobarberbany@gmail.com>
2018-05-15 15:22:12 +0100

    Daemonise deployment controller

--- testDeployment/main.go
+++ testDeployment/main.go
@@ -1,15 +1,26 @@
 package main
 
 import (
-	"fmt"
+	"flag"
 	"github.com/VertebrateResequencing/wr/kubernetes/client"
 	"github.com/VertebrateResequencing/wr/kubernetes/deployment"
+	"github.com/sevlyar/go-daemon"
+	"log"
 	"os"
+	"syscall"
 )
 
-func main() {
-	// Authenticate the client lib with cluster
-	var err error
+var (
+	signal = flag.String("s", "", `send signal to the port forwarding daemon
+		quit -- graceful shutdown 
+		stop -- fast shutdown`)
+)
+
+var err error
+var stopCh chan struct{}
+
+func StartController(stopCh chan struct{}) {
+	// Always authenticate the client lib with cluster
 	c := deployment.Controller{
 		Client: &client.Kubernetesp{},
 	}
@@ -21,8 +32,9 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("Authenticated and Initialised!")
-	fmt.Println("====================")
+	log.Println("Authenticated and Initialised!")
+	log.Println("====================")
+
 	// Create a ConfigMap
 	err = c.Client.CreateInitScriptConfigMap("test", "/Users/tb15/go/src/projects/learning_client_go/testDeployment/test.sh")
 	if err != nil {
@@ -30,8 +42,8 @@ func main() {
 	}
 	// Set up the parameters for the deployment
 	// AttachCmdOpts gets populated by controller when pod is created.
-	fmt.Println("Populating opts")
 	dir, err := os.Getwd()
+	log.Println(dir)
 	if err != nil {
 		panic(err)
 	}
@@ -39,7 +51,7 @@ func main() {
 		ContainerImage: "ubuntu:latest",
 		TempMountPath:  "/wr-tmp",
 		Files: []client.FilePair{
-			{dir + "/wr-linux", "/wr-tmp/"},
+			{"/Users/tb15/go/src/projects/learning_client_go/testDeployment/wr-linux", "/wr-tmp/"},
 		},
 		BinaryPath:      "/scripts/test.sh",
 		BinaryArgs:      []string{"/wr-tmp/wr-linux", "manager", "start", "-f"},
@@ -48,12 +60,61 @@ func main() {
 		RequiredPorts:   []int{1120, 1121},
 	}
 
-	stopCh := make(chan struct{})
 	defer close(stopCh)
-	fmt.Printf("\n\n")
-	fmt.Println("====================")
-	fmt.Printf("\n\n")
-	fmt.Println("Controller started :)")
+	log.Printf("\n\n")
+	log.Println("====================")
+	log.Printf("\n\n")
+	log.Println("Controller started :)")
 
 	c.Run(stopCh)
+
+	return
+}
+
+func main() {
+	flag.Parse()
+	daemon.AddCommand(daemon.StringFlag(signal, "quit"), syscall.SIGQUIT, termHandler)
+	daemon.AddCommand(daemon.StringFlag(signal, "stop"), syscall.SIGTERM, termHandler)
+
+	cntxt := daemon.Context{
+		PidFileName: "pfwpid",
+		PidFilePerm: 0644,
+		LogFileName: "pfwlog",
+		LogFilePerm: 0640,
+		WorkDir:     "/",
+		Umask:       027,
+		Args:        []string{""},
+	}
+
+	// Daemon currently running
+	if len(daemon.ActiveFlags()) > 0 {
+		d, err := cntxt.Search()
+		if err != nil {
+			log.Fatalln("Unable to send signal to daemon: ", err)
+		}
+		daemon.SendCommands(d)
+		return
+	}
+
+	// Check if forward flag is set
+	d, err := cntxt.Reborn()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	if d != nil {
+		return
+	}
+	defer cntxt.Release()
+
+	log.Println("======================")
+	log.Println("daemon started")
+	stopCh = make(chan struct{})
+	StartController(stopCh)
+
+}
+
+func termHandler(sig os.Signal) error {
+	log.Println("terminating portforward....")
+	close(stopCh)
+	return daemon.ErrStop
 }
